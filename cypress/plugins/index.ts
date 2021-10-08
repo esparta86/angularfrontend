bfdff2c52534dd045a9e48079d7a1ae9401eaa11
Débora Guardado <debora.guardado@novatechdev.com>
2021-10-08 10:36:20 -0600

    feat: cypress configuration with eslint and prettier

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 // Plugins enable you to tap into, modify, or extend the internal behavior of Cypress
 // For more info, visit https://on.cypress.io/plugins-api
-module.exports = (on, config) => {}
+module.exports = (on, config) => {};
