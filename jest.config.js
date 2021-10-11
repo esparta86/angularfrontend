6e988b14d788961b66a04dedee81ec8f36f9f278
Débora Guardado <debora.guardado@novatechdev.com>
2021-10-11 14:02:46 -0600

    feat: add circle ci cofig for cypress

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   moduleNameMapper: {
     '@core/(.*)': '<rootDir>/src/app/core/$1',
   },
+  testPathIgnorePatterns: ["<rootDir>/cypress/"],
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   collectCoverage: true,
