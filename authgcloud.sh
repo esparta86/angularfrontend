5e9e8be926c192afd2d73849560042cc9ce889f4
lisandro.rafaelano <lisandro.rafaelano@novatechdev.com>
2021-10-12 22:40:42 -0600

    config.yaml updated

--- authgcloud.sh
+++ authgcloud.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-echo $GCLOUD_SERVICE_KEY > ${HOME}/gcloud-service-key.json
-cat ${HOME}/gcloud-service-key.json
-echo $GCLOUD_SERVICE_KEY
-gcloud auth activate-service-account --key-file=${HOME}/gcloud-service-key.json
+echo $GCLOUD_SERVICE_KEY > gcloud-service-key.json
+
+gcloud auth activate-service-account --key-file=gcloud-service-key.json
 gcloud --quiet config set project ${GOOGLE_PROJECT_ID}
