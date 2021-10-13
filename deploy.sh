2003910294d1e20eccbf9b8d05edcda87d41a1a2
lisandro.rafaelano <lisandro.rafaelano@novatechdev.com>
2021-10-12 22:23:34 -0600

    config.yaml updated

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 echo "Copying build to Storage"
 gsutil defacl ch -u AllUsers:READER gs://$STORAGE_BUCKET
-gsutil rsync -R /tmp/workspace/dist gs://$STORAGE_BUCKET
+gsutil rsync -R /dist gs://$STORAGE_BUCKET
 gsutil setmeta -h "Cache-Control:private, max-age=0, no-transform" \
   gs://$STORAGE_BUCKET/*.js
